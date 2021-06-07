88eca4b6b450315e26589993960d1078cd0e66b4
Ivan Aragonés Muniesa <iaragone@redhat.com>
2021-06-07 15:19:52 +0200

    developing

--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i localhost, worker.yml -K -e '{"vcenter_host": "vcenter.bcnconsulting.com", "user_path": "/home/ivan/GIT/redhat/ITX/ocp4-auto-install/workdir"}'
+ansible-playbook -i localhost, worker.yml -K -e '{"vcenter_hostname": "vcenter.bcnconsulting.com", "user_path": "/home/iaragone/ipi"}'
